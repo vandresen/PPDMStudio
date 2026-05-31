62968a9d4d17e5e3e7623aeb50323adf5fd6f1d1
Vidar Andresen <vidarandresen@yahoo.com>
2026-05-31 14:32:33 -0600

    Fixed marker picks warnings

--- PPDMStudio/Models/MarkerPick.cs
+++ PPDMStudio/Models/MarkerPick.cs
@@ -8,14 +8,14 @@ namespace PPDMStudio.Models
 {
     public class MarkerPick
     {
-        public string UWI { get; set; }
-        public string STRAT_NAME_SET_ID { get; set; }
-        public string STRAT_UNIT_ID { get; set; }
-        public string INTERP_ID { get; set; }
+        public required string UWI { get; set; }
+        public required string STRAT_NAME_SET_ID { get; set; }
+        public required string STRAT_UNIT_ID { get; set; }
+        public required string INTERP_ID { get; set; }
         public double? PICK_DEPTH { get; set; }
         public double? PICK_TVD { get; set; }
         public DateOnly? PICK_DATE { get; set; }
-        public string DOMINANT_LITHOLOGY { get; set; }
-        public string REMARK { get; set; }
+        public string? DOMINANT_LITHOLOGY { get; set; }
+        public string? REMARK { get; set; }
     }
 }
